8641195e9b5be9f8e6bb920093bd0a15260d42cb
s-ys <qkdqkddl3363@naver.com>
2026-06-16 11:19:40 +0900

    PDF Script Code Update

--- PDF Script Code(Viewer alert).py
+++ PDF Script Code(Viewer alert).py
@@ -2,7 +2,7 @@ from pypdf import PdfReader, PdfWriter
 from pypdf.generic import DictionaryObject, NameObject, create_string_object
 
 input_pdf = "example.pdf"   # alert 액션을 추가할 PDF 파일 경로
-output_pdf = "alert_pdf.pdf"    # 자바스크립트 액션이 추가된 PDF 파일 경로
+output_pdf = "alert_pdf_viewer.pdf"    # 자바스크립트 액션이 추가된 PDF 파일 경로
 
 reader = PdfReader(input_pdf)
 writer = PdfWriter()
@@ -15,8 +15,8 @@ for page in reader.pages:
 js_dict = DictionaryObject()
 js_dict.update({
     NameObject("/S"): NameObject("/JavaScript"),
-    NameObject("/JS"): create_string_object("app.alert('PDF 파일이 열렸습니다!');")
-    # 위의 JavaScript 코드는 PDF 파일이 열릴 때 경고창으로 메시지를 표시합니다.
+    NameObject("/JS"): create_string_object("app.alert('Viewer: ' + app.viewerType + '\\nVersion: ' + app.viewerVersion);")
+    # 위의 JavaScript 코드는 PDF 뷰어의 종류와 버전을 경고창으로 표시합니다.
 })
 
 writer._root_object.update({
